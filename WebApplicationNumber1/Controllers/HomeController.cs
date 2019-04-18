5dfe70fd03c48c15ba0917bf32a8c323f36e0390
Maf38 <mafal.gai@outlook.com>
2019-04-18 16:49:36 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplicationNumber1/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationNumber1.Models;
+
+namespace WebApplicationNumber1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string V = "Hello World MVC";
+
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+
+                return View("Error");
+
+            else
+             ViewData["Nom"] = id;
+            ViewBag.prenom = id + "ix";
+            return View();
+        }
+
+        public string Index2()
+
+        {
+            return "Hello les contrôleurs";
+        }
+
+        public string index3(String id)
+        {
+            return "Bonjour " + id;
+        }
+
+        public ActionResult ListeClients()
+
+        {
+
+            Clients clients = new Clients();
+
+            ViewData["Clients"] = clients.ObtenirListeClients();
+
+            return View();
+
+        }
+
+
+        public ActionResult ChercheClient(string id)
+
+        {
+
+            ViewData["Nom"] = id;
+
+            Clients clients = new Clients();
+
+
+            Client client = clients.ObtenirListeClients().FirstOrDefault(c => c.Nom == id);
+
+            if (client != null)
+
+            {
+
+                ViewData["Age"] = client.Age;
+
+                return View("Trouve");
+
+            }
+
+            return View("NonTrouve");
+
+        }
+
+    }
+}
---
+++ WebApplicationNumber1/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationNumber1.Models
+{
+    public class Client
+    {
+        public string Nom { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class Clients
+    {
+        public List<Client> ObtenirListeClients()
+        {
+            return new List<Client>
+        {
+            new Client { Age = 33, Nom = "Nicolas"},
+            new Client { Age = 30, Nom = "Delphine"},
+            new Client { Age = 33, Nom = "Jérémie"},
+            new Client { Age = 1, Nom = "Timéo"}
+        };
+        }
+
+       
+
+    }
+}
